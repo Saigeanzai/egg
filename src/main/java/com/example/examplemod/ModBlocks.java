b640eec605752992f7f2b3a2f8ba209de1b0956c
olive21221 <orp7931@icloud.com>
2022-08-02 15:44:01 -0700

    Aug 2

--- src/main/java/com/example/examplemod/ModBlocks.java
+++ src/main/java/com/example/examplemod/ModBlocks.java
@@ -22,6 +22,17 @@ public static final RegistryObject<Item>MY_PICKAXE = ITEMS.register("my_pickaxe"
     public static final RegistryObject<Item>MY_SHOVEL = ITEMS.register("my_shovel", ()-> new ShovelItem(ItemTier.DIAMOND, 2, -1.08f, new Item.Properties().tab(ItemGroup.TAB_TOOLS)));
     public static final RegistryObject<Item>MY_HOE = ITEMS.register("my_hoe", ()-> new HoeItem(ItemTier.DIAMOND, 1, -1.08f, new Item.Properties().tab(ItemGroup.TAB_TOOLS)));
     public static final RegistryObject<Item>MY_BOW = ITEMS.register("my_bow", ()-> new BowItem( new Item.Properties().tab(ItemGroup.TAB_TOOLS).stacksTo(1)));
+    public static final RegistryObject<Item>MY_FISH = ITEMS.register("my_fish", ()-> new SwordItem(ItemTier.DIAMOND, 20, -1.08f, new Item.Properties().tab(ItemGroup.TAB_TOOLS)));
+    public static final RegistryObject<Item>MY_CAT = ITEMS.register("my_cat", ()-> new PickaxeItem(ItemTier.DIAMOND, 10, -1.08f, new Item.Properties().tab(ItemGroup.TAB_TOOLS)));
+    public static final RegistryObject<Item>MY_DOG = ITEMS.register("my_dog", ()-> new AxeItem(ItemTier.DIAMOND, 21, -1.08f, new Item.Properties().tab(ItemGroup.TAB_TOOLS)));
+    public static final RegistryObject<Item>MY_MOUSE = ITEMS.register("my_mouse", ()-> new HoeItem(ItemTier.DIAMOND, 5, -1.08f, new Item.Properties().tab(ItemGroup.TAB_TOOLS)));
+    public static final RegistryObject<Item>BIG_RABBIT = ITEMS.register("big_rabbit", ()-> new ShovelItem(ItemTier.DIAMOND, 6, -1.08f, new Item.Properties().tab(ItemGroup.TAB_TOOLS)));
+    public static final RegistryObject<Item> PANCAKE=ITEMS.register("pancake",()-> new Item(new Item.Properties().tab(ItemGroup.TAB_MISC)));
+    public static final RegistryObject<Block> MY_BLOCK = BLOCKS.register("my_block", ()-> new Block(AbstractBlock.Properties.of(Material.WOOL)));
+    public static final RegistryObject<Item>MY_BOU = ITEMS.register("my_bou", ()-> new BowItem( new Item.Properties().tab(ItemGroup.TAB_TOOLS).stacksTo(1)));
+    public static final RegistryObject<Item>MY_BOWW = ITEMS.register("my_boww", ()-> new BowItem( new Item.Properties().tab(ItemGroup.TAB_TOOLS).stacksTo(2)));
+    public static final RegistryObject<Item>MY_BOWWW = ITEMS.register("my_bowww", ()-> new BowItem( new Item.Properties().tab(ItemGroup.TAB_TOOLS).stacksTo(2)));
+    public static final RegistryObject<Item>MY_TRIDENT = ITEMS.register("my_trident", ()-> new BowItem( new Item.Properties().tab(ItemGroup.TAB_TOOLS).stacksTo(2)));
 
 
 }
